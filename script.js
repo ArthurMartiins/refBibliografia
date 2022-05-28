44ac219dd38f15f9c3d73f1bdfe4384e14314982
Arthur Martins Zimmermann <72109938+ArthurMartiins@users.noreply.github.com>
2022-05-28 01:43:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// var moment = require("moment");
-// moment().format("DD/MM/YYY");
-
 var nome = document.getElementById("nome");
 const element = document.getElementById("myBtn");
 element.addEventListener("click", myFunction);
 
-function sentenceCase(str) {
-  return str
-    .replace(/[a-z]/i, function (letter) {
-      return letter.toUpperCase();
-    })
-    .trim();
-}
+// const meses = [
+//   "Jan",
+//   "Fev",
+//   "Mar",
+//   "Abr",
+//   "Mai",
+//   "Jun",
+//   "Jul",
+//   "Ago",
+//   "Set",
+//   "Out",
+//   "Nov",
+//   "Dez",
+// ];
+// let data = new Date();
+// let dataFormatada =
+//   data.getDate() + " " + meses[data.getMonth()] + " " + data.getFullYear();
+// console.log(dataFormatada);
 
 function myFunction() {
   document.getElementById(
@@ -21,7 +29,10 @@ function myFunction() {
   }. ${tituloArt.value.bold()}. ${url.value}. Acesso em: ${data.value}.`;
 }
 
-document.getElementById("copiar").addEventListener("click", copiar);
+var copiarRef = document.getElementById("copiar");
+if (copiarRef) {
+  copiarRef.addEventListener("click", copiar);
+}
 
 function copiar() {
   var copyText = document.getElementById("demo");
@@ -33,3 +44,25 @@ function copiar() {
   textArea.remove();
   alert("texto copiado");
 }
+
+console.log(`
+  z$$$$$. $$
+  $$$$$$$$$$$
+ $$$$$$**$$$$             eeeeer
+$$$$$%   '$$$             $$$$$F
+4$$$$P     *$$             *$$$$F
+$$$$$      '$$    .ee.      ^$$$F            ..e.
+$$$$$       ""  .$$$$$$b     $$$F 4$$$$$$   $$$$$$c
+4$$$$F          4$$$""$$$$    $$$F '*$$$$*  $$$P"$$$L
+4$$$$F         .$$$F  ^$$$b   $$$F  J$$$   $$$$  ^$$$.
+4$$$$F         d$$$    $$$$   $$$F J$$P   .$$$F   $$$$
+4$$$$F         $$$$    3$$$F  $$$FJ$$P    4$$$"   $$$$
+4$$$$F        4$$$$    4$$$$  $$$$$$$r    $$$$$$$$$$$$  ine
+4$$$$$        4$$$$    4$$$$  $$$$$$$$    $$$$********
+$$$$$        4$$$$    4$$$F  $$$F4$$$b   *$$$r
+3$$$$F       d$$$$    $$$$"  $$$F *$$$F  4$$$L     .
+$$$$$.     d$$$$$.   $$$$   $$$F  $$$$.  $$$$    z$P
+ $$$$$e..d$$$"$$$b  4$$$"  J$$$L  '$$$$  '$$$b..d$$
+  *$$$$$$$$$  ^$$$be$$$"  $$$$$$$  3$$$$F "$$$$$$$"
+   ^*$$$$P"     *$$$$*    $$$$$$$   $$$$F  ^*$$$"
+`);
